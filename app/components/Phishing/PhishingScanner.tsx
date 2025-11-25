d2df439a3c6c2a9509ff89dd06ab8edefc5b83ca
Khem Khem <khem6333@gmail.com>
2025-11-25 10:23:14 +0100

    Fix url

--- app/components/Phishing/PhishingScanner.tsx
+++ app/components/Phishing/PhishingScanner.tsx
@@ -21,7 +21,7 @@ const PhishingScanner = () => {
     setResult(null);
 
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_API_URL!, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL!}/scan/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
